d5102e009fceb73bd7d932bc43cfcbaba031c2ce
Barış Demirel <bdemrel732@gmail.com>
2015-09-29 10:59:57 +0300

    add new routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var morgan = require('morgan'),
     express = require('express'),
-    app = express();
+    app = express(),
+    login = require('/routes/login.js');
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/public'));
+app.use('/login', login);
 
 app.listen('8080');
