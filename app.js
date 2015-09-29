d97da17f9f37be81e111c5306b98faa911f53a69
bdemirel <bdemrel732@gmail.com>
2015-09-29 11:09:37 +0300

    rename files

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var morgan = require('morgan'),
     express = require('express'),
     app = express(),
+    index = require('/routes/index.js'),
     login = require('/routes/login.js');
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/public'));
+app.use('/', index);
 app.use('/login', login);
 
 app.listen('8080');
